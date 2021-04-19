a18154c41eb85bbf727d16569f610bab6272c6f1
Fábio Mendes <fabiobends@gmail.com>
2021-04-19 17:43:58 -0300

    Add first day content

---
+++ lib/screens/home/home_screen.dart
@@ -0,0 +1,18 @@
+import 'package:DevQuiz/screens/home/widgets/appbar/app_bar.dart';
+import 'package:flutter/material.dart';
+
+class HomeScreen extends StatefulWidget {
+  HomeScreen({Key? key}) : super(key: key);
+
+  @override
+  _HomeScreenState createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBarWidget(),
+    );
+  }
+}
